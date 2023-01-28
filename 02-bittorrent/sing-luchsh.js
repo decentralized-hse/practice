aa975ebdb0b8cbc86ebe0fd70c7b4dfcc6f561f7
ch0p1k3 <ch00p.228@gmail.com>
2023-01-28 11:55:05 +0300

    sign-luchsh.js

---
+++ 02-bittorrent/sing-luchsh.js
@@ -0,0 +1,26 @@
+var ed25519 = require('ed25519');
+var fs = require('fs');
+var path = require('path');
+var crypto = require('crypto')
+
+function main() {
+    if (process.argv.length < 3) {
+        throw new Error("Not enough arguments");
+    }
+
+    const filePath = path.join(__dirname, `${process.argv[2]}.root`);
+    const secPath = path.join(__dirname, '.sec');
+    const pubPath = path.join(__dirname, '.pub');
+    const signaturePath = path.join(__dirname, `${process.argv[2]}.sign`);
+
+    const file = fs.readFileSync(filePath);
+    const keyPair = ed25519.MakeKeypair(crypto.randomBytes(32));
+    fs.writeFileSync(secPath, keyPair.privateKey);
+    fs.writeFileSync(pubPath, keyPair.publicKey);
+
+    signature = ed25519.Sign(file, keyPair);
+
+    fs.writeFileSync(signaturePath, signature.toString());
+}
+
+main()
