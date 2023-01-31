3e96945e2d6bc1407ea760c415e9335c8b283fae
akimov2100@gmail.com <akimov2100@gmail.com>
2023-01-31 23:19:50 +0300

    correct_version

--- 02-bittorrent/sing-akimov.go
+++ 02-bittorrent/sing-akimov.go
@@ -3,23 +3,49 @@ package main
 import (
 	"crypto/ed25519"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
-	var str_hash string
-	fmt.Fscan(os.Stdin, &str_hash)
+	// input namefile
+	name_file := os.Args[1]
 
-	// tree hash
+	// input hash
+	file, err := os.Open(name_file + ".root")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	data := make([]byte, 64)
+	var str_hash string
+
+	for {
+		n, err1 := file.Read(data)
+		if err1 == io.EOF {
+			break
+		}
+		str_hash = str_hash + string(data[:n])
+	}
+
 	hash := []byte(str_hash)
 
 	// generate pub and prv key
 	publ, priv, _ := ed25519.GenerateKey(nil)
 
-	// sign
-	sig := ed25519.Sign(priv, hash[:])
+	// output keys
+	file, _ = os.Create(name_file + ".public_key")
+	file.WriteString(string(publ))
 
-	fmt.Printf("Public key = \t%x\n\n", publ)
-	fmt.Printf("Private key = \t%x\n\n", priv)
-	fmt.Printf("Signature = \t%x\n\n", sig)
+	file, _ = os.Create(name_file + ".private_key")
+	file.WriteString(string(priv))
+
+	// sign
+	sign := ed25519.Sign(priv, hash[:])
+
+	// output sign
+	file, _ = os.Create(name_file + ".sign")
+	defer file.Close()
+	file.WriteString(string(sign))
 }
