bf71b074ef5adb9f2644dc8c2c3cbe9263e9c492
Victor Grishchenko <gritzko@ya.ru>
2023-02-05 07:18:29 +0300

    corrections

--- 02-bittorrent/proof-azam.js
+++ 02-bittorrent/proof-azam.js
@@ -10,8 +10,8 @@ const block = parseInt(process.argv[3]);
 var node = block * 2;
 
 console.log(`reading ${process.argv[2]}.hashtree...`);
-const hashtree = fs.readFileSync(fileR, 'utf8').split(' ');
-var len = hashtree.length;
+const hashtree = fs.readFileSync(fileR, 'utf8').split('\n');
+var len = hashtree.length-1;
 
 var res = []
 for (let i = 1; true; i++) {
@@ -24,8 +24,9 @@ for (let i = 1; true; i++) {
 	node = par;
 }
 
-console.log(`putting the proof into ${process.argv[2]}.proof...`)
-const fileW = path.join(__dirname, `${process.argv[2]}.proof`);
-fs.writeFileSync(fileW, res.join('\n'))
+var outfn = process.argv[2] + "." + block + ".proof";
+console.log(`putting the proof into ${outfn}...`)
+const fileW = path.join(__dirname, `${outfn}`);
+fs.writeFileSync(fileW, res.join('\n')+"\n")
 
 console.log("all done!");
