6c8e77d0b0687eda5aa4e46915ae8d2dd4dab5fd
defunator <defunator@yandex-team.ru>
2023-02-05 13:59:06 +0300

    style

--- 02-bittorrent/defunator-hashtree.py
+++ 02-bittorrent/defunator-hashtree.py
@@ -7,26 +7,26 @@ BLOCK_SIZE = 1024
 HASH = lambda x : hashlib.sha256(x).hexdigest()
 
 
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str)
-    args = parser.parse_args()
-
-    input_filename = args.input
-
+def read_bytes(filename):
     with open(input_filename, "rb") as f:
         bytes = f.read()
 
     if len(bytes) % BLOCK_SIZE != 0:
         print(f"Failed to chunk file into blocks: {len(bytes)} % {BLOCK_SIZE} != 0")
         exit(0)
+    
+    return bytes
 
+def hash_leaves(bytes):
     leave_hashes = []
+
     for start_index in range(0, len(bytes), BLOCK_SIZE):
         end_index = start_index + BLOCK_SIZE
         leave_hashes.append(HASH(bytes[start_index:end_index]))
 
+    return leave_hashes
+
+def construct_hash_tree(leave_hashes):
     hash_tree_size = len(leave_hashes) * 2 - 1
     hash_tree = ["0" * hashlib.sha256().block_size] * hash_tree_size
 
@@ -41,6 +41,21 @@ if __name__ == "__main__":
             hash_tree_index = 2 ** (level + 1) * node_index + 2 ** level - 1
             left_hash, right_hash = hash_tree[hash_tree_index - 2 ** (level - 1)], hash_tree[hash_tree_index + 2 ** (level - 1)]
             hash_tree[hash_tree_index] = HASH(f"{left_hash}\n{right_hash}\n".encode("UTF-8"))
+    
+    return hash_tree
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=str)
+    args = parser.parse_args()
+
+    input_filename = args.input
+    bytes = read_bytes(input_filename)
+    
+    leave_hashes = hash_leaves(bytes)
+    hash_tree = construct_hash_tree(leave_hashes)
 
     with open(f"{input_filename}.hashtree", "w") as f:
         f.writelines("\n".join(hash_tree))
