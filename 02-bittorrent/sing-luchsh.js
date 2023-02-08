0b0cbc48ef35d3ecfad5e6ea8db2d1e4262ac1b7
ch0p1k3 <ch00p.228@gmail.com>
2023-02-09 00:23:39 +0300

    fix issues

--- 02-bittorrent/sing-luchsh.js
+++ 02-bittorrent/sing-luchsh.js
@@ -3,24 +3,40 @@ var fs = require('fs');
 var path = require('path');
 var crypto = require('crypto')
 
+function buf2hex(buffer) {
+  const byteArray = new Uint8Array(buffer);
+
+  const hexParts = [];
+  for (let i = 0; i < byteArray.length; i++) {
+    const hex = byteArray[i].toString(16);
+
+    const paddedHex = ('00' + hex).slice(-2);
+
+    hexParts.push(paddedHex);
+  }
+
+  return hexParts.join('');
+}
+
+
 function main() {
-    if (process.argv.length < 3) {
-        throw new Error("Not enough arguments");
-    }
+  if (process.argv.length < 3) {
+    throw new Error('Not enough arguments');
+  }
 
-    const filePath = path.join(__dirname, `${process.argv[2]}.root`);
-    const secPath = path.join(__dirname, '.sec');
-    const pubPath = path.join(__dirname, '.pub');
-    const signaturePath = path.join(__dirname, `${process.argv[2]}.sign`);
+  const filePath = path.join(__dirname, `${process.argv[2]}.root`);
+  const secPath = path.join(__dirname, `${process.argv[2]}.sec`);
+  const pubPath = path.join(__dirname, `${process.argv[2]}.pub`);
+  const signaturePath = path.join(__dirname, `${process.argv[2]}.sign`);
 
-    const file = fs.readFileSync(filePath);
-    const keyPair = ed25519.MakeKeypair(crypto.randomBytes(32));
-    fs.writeFileSync(secPath, keyPair.privateKey);
-    fs.writeFileSync(pubPath, keyPair.publicKey);
+  const file = fs.readFileSync(filePath);
+  const keyPair = ed25519.MakeKeypair(crypto.randomBytes(32));
+  fs.writeFileSync(secPath, buf2hex(keyPair.privateKey));
+  fs.writeFileSync(pubPath, buf2hex(keyPair.publicKey));
 
-    signature = ed25519.Sign(file, keyPair);
+  signature = ed25519.Sign(file, keyPair);
 
-    fs.writeFileSync(signaturePath, signature);
+  fs.writeFileSync(signaturePath, buf2hex(signature));
 }
 
 main();
