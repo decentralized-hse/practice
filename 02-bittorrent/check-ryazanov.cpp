77684a54bfa500bd2bd9da193435197891325f57
Rybbba <rybbba@gmail.com>
2023-02-09 15:07:25 +0300

    Use hex format

--- 02-bittorrent/check-ryazanov.cpp
+++ 02-bittorrent/check-ryazanov.cpp
@@ -5,6 +5,29 @@
 
 using namespace std;
 
+char h2c(char h) {
+    h = tolower(h);
+    if ('0' <= h && h <= '9') {
+        return h - '0';
+    }
+    if ('a' <= h && h <= 'f') {
+        return 10 + h - 'a';
+    }
+    throw invalid_argument("Bad character for hex");
+}
+
+string hex2b(const string& hex_str) {
+    if (hex_str.size() % 2 != 0) {
+        throw invalid_argument("Bad size for hex string");
+    }
+    string b_string;
+    b_string.reserve(hex_str.size()/2);
+    for (size_t i = 0; i < hex_str.size(); i += 2) {
+        b_string.push_back(h2c(hex_str[i])*16 + h2c(hex_str[i+1]));
+    }
+    return b_string;
+}
+
 int main(int argc, char *argv[]) {
     if (sodium_init() < 0) {
         cout << "Sodium couldn't be initialized.\n";
@@ -24,20 +47,22 @@ int main(int argc, char *argv[]) {
         cout << "Public key file not found.\n";
         return 1;
     }
-    stringstream keyb;
-    keyb << keyfile.rdbuf();
+    string key_hex;
+    keyfile >> key_hex;
+    string keyb = hex2b(key_hex);
 
     ifstream signfile(string(argv[1]) + ".sign");
     if (!signfile) {
         cout << "Signature key file not found.\n";
         return 1;
     }
-    stringstream signb;
-    signb << signfile.rdbuf();
+    string sign_hex;
+    signfile >> sign_hex;
+    string signb = hex2b(sign_hex);
 
-    int verified = crypto_sign_ed25519_verify_detached((const unsigned char *)(signb.str().c_str()),
+    int verified = crypto_sign_ed25519_verify_detached((const unsigned char *)(signb.c_str()),
             (const unsigned char *)(mb.str().c_str()), mb.str().size(),
-            (const unsigned char *)(keyb.str().c_str()));
+            (const unsigned char *)(keyb.c_str()));
     if (verified == 0) {
         cout << "Message signature is correct!\n";
         return 0;
