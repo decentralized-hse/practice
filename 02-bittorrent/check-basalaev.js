5b6325b76056f14e53b0afd06513d7fa4aa2d7b9
Maksim <p3tyn19@gmail.com>
2023-02-09 23:16:43 +0300

    fix

--- 02-bittorrent/check-basalaev.js
+++ 02-bittorrent/check-basalaev.js
@@ -12,8 +12,10 @@ function main() {
     const signPath = path.join(__dirname, `${process.argv[2]}.sign`);
 
     try {
-        var b64PubKey = fs.readFileSync(pubKeyPath);
-        var pubKey = Buffer.from(b64PubKey, 'base64');
+        var hexPubKey = fs.readFileSync(pubKeyPath).toString();
+        var pubKey = new Uint8Array(hexPubKey.match(/[\da-f]{2}/gi).map(function (h) {
+            return parseInt(h, 16)
+        }))
     } catch (e) {
         console.log('Failed to read file "%s":\n%s', pubKeyPath, e);
         process.exit(1);
@@ -27,8 +29,10 @@ function main() {
     }
 
     try {
-        var b64Sign = fs.readFileSync(signPath);
-        var sign = Buffer.from(b64Sign, 'base64');
+        var hexSign = fs.readFileSync(signPath).toString();
+        var sign = new Uint8Array(hexSign.match(/[\da-f]{2}/gi).map(function (h) {
+            return parseInt(h, 16)
+        }))
     } catch (e) {
         console.log('Failed to read file "%s":\n%s', pubKeyPath, e);
         process.exit(1);
