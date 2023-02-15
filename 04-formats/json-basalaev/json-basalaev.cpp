29fc8ef9abb6ac5517746ff944ded94c4867ae0f
Maksim <p3tyn19@gmail.com>
2023-02-16 01:04:54 +0300

    add json lib

--- 04-formats/json-basalaev/json-basalaev.cpp
+++ 04-formats/json-basalaev/json-basalaev.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdint>
 #include <cstdio>
 #include <cstring>
 #include <fstream>
@@ -6,6 +7,9 @@
 #include <set>
 #include <string>
 #include <vector>
+#include "json.hpp"
+
+using namespace nlohmann;
 
 struct Student {
     char    name[32];
@@ -56,40 +60,25 @@ void writeStudentsIntoJson(const std::vector<Student>& students, const TaskAux& 
         out << "\"" << field << "\": " << "\"" << str << "\",\n";
     };
 
-    out << "[\n";
+    json result;
     for (auto i = 0; i < students.size(); ++i) {
-        if (i > 0) {
-            out << ",\n";
-        }
-        out << "{\n";
+        json currentStudent;
 
-        // dump strs
-        wrapStr("name", students[i].name, out);
-        wrapStr("login", students[i].login, out);
-        wrapStr("group", students[i].group, out);
+        currentStudent["name"] = students[i].name;
+        currentStudent["login"] = students[i].login;
+        currentStudent["group"] = students[i].group;
 
-        // dump practice
-        out << "\"practice\": [";
         for (auto j = 0; j < students[i].practiceSize; j++) {
-            if (j > 0) {
-                out << ",";
-            }
-            out << std::to_string(students[i].practice[j]);
+            currentStudent["practice"].push_back(std::to_string(students[i].practice[j]));
         }
-        out << "],\n";
 
-        // dump project
-        out << "\"project\": \n{\n";
-        wrapStr("repo", students[i].project.repo, out);
-        out << "\"mark\": " << static_cast<uint32_t>(students[i].project.mark) << "\n";
-        out << "},\n";
+        currentStudent["project"] = {{"repo", students[i].project.repo}, {"mark", static_cast<uint32_t>(students[i].project.mark)}};
+        currentStudent["mark"] = students[i].mark;
 
-        // dump float
-        out << "\"mark\": " << students[i].mark << "\n";
-
-        out << "}";
+        result.push_back(currentStudent);
     }
-    out << "\n]\n";
+
+    out << result;
 
     std::cout << "Written to " << taskAux.outputFilename;
 }
@@ -99,23 +88,7 @@ void covertBinaryToJson(const TaskAux& taskAux) {
     writeStudentsIntoJson(students, taskAux);
 }
 
-void clearString(std::string& str) {
-    const std::set<char> permitedSymbols{'\"', ',', '[', ']'};
-    while (std::isspace(str.back()) || permitedSymbols.count(str.back())) {
-        str.pop_back();
-    }
-
-    std::reverse(str.begin(), str.end());
-
-    while (std::isspace(str.back()) || permitedSymbols.count(str.back())) {
-        str.pop_back();
-    }
-
-    std::reverse(str.begin(), str.end());
-}
-
-void parseString(std::string& str, char out[]) {
-    clearString(str);
+void parseString(const std::string& str, char out[]) {
     memcpy(out, str.c_str(), str.size());
 }
 
@@ -124,65 +97,26 @@ std::vector<Student> readJsonStudents(const TaskAux& taskAux) {
               << taskAux.inputFilename << std::endl;
 
     std::ifstream in(taskAux.inputFilename);
+    json j = json::parse(in);
 
     std::vector<Student> result;
-    std::string line;
-    bool projectFlag = false;
     Student student{};
-    int bracketBalance = 0;
-    while (getline(in, line)) {
-        if (line == "{") {
-            bracketBalance++;
-            continue;
-        }
 
-        if (line == "}," || line == "}") {
-            bracketBalance--;
-            if (!bracketBalance) {
-                result.emplace_back(student);
-                student = Student{};
-            }
-            continue;
+    for (const auto& jsonStudent : j) {
+        parseString(jsonStudent["name"].get<std::string>(), student.name);
+        parseString(jsonStudent["login"].get<std::string>(), student.login);
+        parseString(jsonStudent["group"].get<std::string>(), student.group);
+        parseString(jsonStudent["project"]["repo"].get<std::string>(), student.project.repo);
+        student.project.mark = jsonStudent["project"]["mark"].get<uint8_t>();
+        student.mark = jsonStudent["mark"].get<float>();
+        for (auto i = 0; i < jsonStudent["practice"].size(); ++i) {
+            if (jsonStudent["practice"][i].get<std::string>() == "0")
+                student.practice[i] = 0;
+            else
+                student.practice[i] = 1;
         }
-
-        auto delimeter = line.find_first_of(":");
-        if (delimeter == std::string::npos) {
-            continue;
-        }
-
-        std::string fieldName = line.substr(1, delimeter - 2);
-        std::string fieldValue = line.substr(delimeter + 1);
-
-        if (fieldName == "project") {
-            projectFlag = true;
-            continue;
-        }
-
-        if (fieldName == "name") {
-            parseString(fieldValue, student.name);
-        } else if (fieldName == "login") {
-            parseString(fieldValue, student.login);
-        } else if (fieldName == "group") {
-            parseString(fieldValue, student.group);
-        } else if (fieldName == "repo") {
-            parseString(fieldValue, student.project.repo);
-        } else if (fieldName == "mark") {
-            clearString(fieldValue);
-            if (projectFlag) {
-                student.project.mark = std::stoul(fieldValue);
-                projectFlag = false;
-            } else {
-                student.mark = std::stof(fieldValue);
-            }
-        } else if (fieldName == "practice") {
-            clearString(fieldValue);
-            size_t idx = 0;
-            for (auto i = 0; i < fieldValue.size(); ++i) {
-                if (fieldValue[i] == '0' || fieldValue[i] == '1') {
-                    student.practice[idx++] = fieldValue[i] - '0';
-                }
-            }
-        }
+        result.push_back(student);
+        student = Student{};
     }
 
     std::cout << result.size() << " students read..." << std::endl;
