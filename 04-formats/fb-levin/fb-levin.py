e2555e9d264e8077b4a2085be70e634aea63efdb
dopeyhex <dopeyhex@outlook.com>
2023-02-22 09:12:18 +0300

    initial commit

---
+++ 04-formats/fb-levin/fb-levin.py
@@ -0,0 +1,16 @@
+import os
+import schema
+import struct
+import sys
+
+def get_file_type(file_name):
+    return file_name.split('.')[-1]
+
+path = sys.argv[1]
+file_type = get_file_type(path)
+if file_type == PROTOBUF_EXTENSION:
+    fb_to_bin(sys.argv[1])
+elif file_type == BIN_EXTENSION:
+    bin_to_fb(sys.argv[1])
+else:
+    print("Bad format!")
