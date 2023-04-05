7b87b907e1f0d8af831cac8a99d3ca4b3946e033
dopeyhex <dopeyhex@outlook.com>
2023-04-05 22:40:27 +0300

    Yet another try

--- 04-formats/fb-levin/fb-levin.py
+++ 04-formats/fb-levin/fb-levin.py
@@ -6,75 +6,82 @@ import sys
 
 student_format = '<32s16s8s8B59sBf'
 
-def read_flat(file):
-    buf = file.read()
+def flat_to_bin(input_file, output_file):
+    while True:
+        size_data = input_file.read(4)
+        if not size_data:
+            break
+
+        size = struct.unpack('<i', size_data)[0]
+        buf = input_file.read(size)
+
+        root = Root.Root.GetRootAsRoot(buf, 0)
+
+        for i in range(root.StudentsLength()):
+            student = root.Students(i)
+
+            name = btos(student.Name())
+            login = btos(student.Login())
+            group = btos(student.Group())
+            practice = [student.Practice(j) for j in range(student.PracticeLength())]
+            repo = btos(student.Project().Repo())
+            project_mark = student.Project().Mark()
+            mark = student.Mark()
+
+            student_data = struct.pack(
+                student_format,
+                bytes(name, 'utf-8'),
+                bytes(login, 'utf-8'),
+                bytes(group, 'utf-8'),
+                *practice,
+                bytes(repo, 'utf-8'),
+                project_mark,
+                mark
+            )
+            output_file.write(struct.pack('<i', len(student_data)))
+            output_file.write(student_data)
+
+def bin_to_flat(input_file, output_file):
     students = []
+    while True:
+        student_data = input_file.read(struct.calcsize(student_format))
+        if not student_data:
+            break
 
-    root = Root.Root.GetRootAsRoot(buf, 0)
-    students_len = root.StudentsLength()
-    for i in range(students_len):
-        student_flat = Student.Student()
-        if not root.Students(i):
-            raise ValueError(f"no {i} student")
+        name, login, group, *practice, repo, project_mark, mark = struct.unpack(student_format, student_data)
 
-        student = Student.Student()
-        student.Name = student_flat.Name().decode('utf-8')
-        student.Login = student_flat.Login().decode('utf-8')
-        student.Group = student_flat.Group().decode('utf-8')
+        builder = flatbuffers.Builder(2048)
 
-        practice_len = student_flat.PracticeLength()
-        for j in range(practice_len):
-            student.practice[j] = student_flat.Practice(j)
-
-        Project = Project()
-        if student_flat.Project(Project) is None:
-            raise ValueError(f"no {i} student's Project")
-
-        student.Project.repo = Project.Repo().decode('utf-8')
-        student.Project.Mark = Project.Mark()
-
-        student.Mark = student_flat.Mark()
-
-        students.append(student)
-    return students
-
-def write_flat(file, students : list[Student.Student]):
-    builder = flatbuffers.Builder(2048)
-    students_tables = []
-
-    for st in students:
-        if type(st) != Student:
-            print(st)
-            continue
-        Name = builder.CreateString(btos(st.Name))
-        Login = builder.CreateString(btos(st.Login))
-        Group = builder.CreateString(btos(st.Group))
+        Name = builder.CreateString(btos(name))
+        Login = builder.CreateString(btos(login))
+        Group = builder.CreateString(btos(group))
 
         Student.StudentStartPracticeVector(builder, 8)
 
-        for i in range(len(st.practice)):
-            builder.PrependByte(st.practice[len(st.practice)-1-i])
+        for i in range(len(practice)):
+            builder.PrependByte(practice[len(practice)-1-i])
 
-        practice = builder.EndVector(8)
-        repo = builder.CreateString(btos(st.Project.repo))
+        prac = builder.EndVector(8)
+        repo = builder.CreateString(btos(repo))
 
         Project.ProjectStart(builder)
         Project.ProjectAddRepo(builder, repo)
-        Project.ProjectAddMark(builder, st.Project.Mark)
+        Project.ProjectAddMark(builder, project_mark)
+        project_obj = Project.ProjectEnd(builder)
 
-        Project = Project.ProjectEnd(builder)
-        Project.StudentStart(builder)
-        Project.StudentAddName(builder, Name)
-        Project.StudentAddLogin(builder, Login)
-        Project.StudentAddGroup(builder, Group)
-        Project.StudentAddPractice(builder, practice)
-        Project.StudentAddProject(builder, Project)
-        Project.StudentAddMark(builder, st.Mark)
-        students_tables.append(Student.StudentEnd(builder))
+        Student.StudentStart(builder)
+        Student.StudentAddName(builder, Name)
+        Student.StudentAddLogin(builder, Login)
+        Student.StudentAddGroup(builder, Group)
+        Student.StudentAddPractice(builder, prac)
+        Student.StudentAddProject(builder, project_obj)
+        Student.StudentAddMark(builder, mark)
 
+        students.append(Student.StudentEnd(builder))
+    
     Root.RootStartStudentsVector(builder, len(students))
-    for i in range(len(students_tables)):
-        builder.PrependUOffsetT(students_tables[len(students_tables)-1-i])
+    for i in range(len(students)):
+        builder.PrependVOffsetT(students[len(students)-1-i])
 
     students_vec = builder.EndVector(len(students))
 
@@ -84,27 +91,10 @@ def write_flat(file, students : list[Student.Student]):
 
     builder.Finish(root)
     buf = builder.Output()
-    file.write(buf)
-
-def read_bin(file):
-    students = []
-    while True:
-        student_data = file.read(struct.calcsize(student_format))
-        if not student_data:
-            return students
-        student = Student.Student.GetRootAs(student_data)
-        students.append(student)
-
-def write_bin(file, students):
-    for student in students:
-        student_data = struct.pack(student_format, *student)
-        file.write(student_data)
+    output_file.write(buf)
 
 def btos(s):
-    i = s.index(0)
-    if i != -1:
-        return s[:i].decode('utf-8')
-    return s.decode('utf-8')
+    return s.decode('utf-8').rstrip('\0')
 
 def output(path):
     file = open(path, 'wb')
@@ -114,16 +104,13 @@ def main():
     input_file = open(sys.argv[1], 'rb')
     input_file_extension = os.path.splitext(sys.argv[1])[1]
     if input_file_extension == '.bin':
-        student = read_bin(input_file)
         output_path = os.path.splitext(sys.argv[1])[0] + '.flat'
         output_file = output(output_path)
-        write_flat(output_file, student)
+        bin_to_flat(input_file, output_file)
     elif input_file_extension == '.flat':
-        student = read_flat(input_file)
         output_path = os.path.splitext(sys.argv[1])[0] + '.bin'
-        print(output_path)
         output_file = output(output_path)
-        write_bin(output_file, student)
+        flat_to_bin(input_file, output_file)
     else:
         print('Invalid file extension')
     input_file.close()
