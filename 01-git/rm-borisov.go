0d914ebfa7c6b96373d0e6d075f7859208144b37
Artyom Borisov <2001raf@gmail.com>
2023-10-14 21:07:07 +0500

    Added a validation of the name and hash of a tree.

--- 01-git/rm-borisov.go
+++ 01-git/rm-borisov.go
@@ -33,9 +33,34 @@ func createTreeLine(objectName string, objectSha256Hash string) string {
 	return strings.Join([]string{objectName, objectSha256Hash}, "\t")
 }
 
+func isCharHex(char rune) bool {
+	return ('0' <= char && char <= '9' || 'a' <= char && char <= 'f' || 'A' <= char && char <= 'F')
+}
+
+func isStringHex(str string) bool {
+	for _, char := range str {
+		if !isCharHex(char) {
+			return false
+		}
+	}
+	return true
+}
+
+func isStringSha256Hash(str string) bool {
+	return len(str) == 64 && isStringHex(str)
+}
+
 func readSortedTreeLines(treeSha256Hash string) []string {
+	if !isStringSha256Hash(treeSha256Hash) {
+		fmt.Fprintf(os.Stderr, "Error: %s isn't a SHA256 hash.\n", treeSha256Hash)
+		os.Exit(1)
+	}
 	treeByteContent, err := os.ReadFile(treeSha256Hash)
 	exitIfError(err)
+	if getHexEncodedSha256Hash(treeByteContent) != treeSha256Hash {
+		fmt.Fprintf(os.Stderr, "Error: the hash of the %s tree isn't equal to its name.\n", treeSha256Hash)
+		os.Exit(1)
+	}
 	return strings.Split(string(treeByteContent), "\n")
 }
 
@@ -55,14 +80,14 @@ func getObjectIndexInTree(sortedTreeLines []string, objectName string) int {
 	return objectIndex
 }
 
-func GetHexEncodedSha256Hash(bytesToHash []byte) string {
+func getHexEncodedSha256Hash(bytesToHash []byte) string {
 	sha256HashBytes := sha256.Sum256(bytesToHash)
 	return hex.EncodeToString(sha256HashBytes[:])
 }
 
 func createTree(sortedTreeLines []string) string {
 	newTreeByteContent := []byte(strings.Join(sortedTreeLines, "\n"))
-	newTreeSha256Hash := GetHexEncodedSha256Hash(newTreeByteContent)
+	newTreeSha256Hash := getHexEncodedSha256Hash(newTreeByteContent)
 	err := os.WriteFile(newTreeSha256Hash, newTreeByteContent, 0644)
 	exitIfError(err)
 	return newTreeSha256Hash
@@ -123,7 +148,7 @@ func isObjectPathCharValid(char rune) bool {
 	return true
 }
 
-func verifyObjectPath(objectPath string) {
+func verifyObjectPathChars(objectPath string) {
 	for _, ch := range objectPath {
 		if !isObjectPathCharValid(ch) {
 			fmt.Fprintf(os.Stderr, "Error: the object path contains the invalid character '%c'.\n", ch)
@@ -135,7 +160,7 @@ func verifyObjectPath(objectPath string) {
 func main() {
 	args := os.Args
 	verifyArgsLength(args)
-	verifyObjectPath(args[1])
+	verifyObjectPathChars(args[1])
 	objectNames := getObjectsNames(args[1])
 	fmt.Print(removeObjectFromTreeRecursively(args[2], objectNames))
 }
