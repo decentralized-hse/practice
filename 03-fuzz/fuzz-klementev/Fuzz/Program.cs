7aa95caffcbbbfc8bbe3a4b4f3e25964f1650d47
Клементьев Матвей <klementevlogotip@mail.ru>
2023-10-30 01:39:41 +0500

    kv-triapitsyn fuzzing

---
+++ 03-fuzz/fuzz-klementev/Fuzz/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ParseLibrary;
+using SharpFuzz;
+
+// Fuzzer.OutOfProcess.Run(stream =>
+// {
+//     try
+//     {
+//         using (var reader = new StreamReader(stream))
+//         {
+//             MainParser.FromKvFile(reader);
+//             // JSON.DeserializeDynamic(reader);
+//         }
+//     }
+//     catch (ParseException e)
+//     {
+//     }
+// });
+
+
+try
+{
+    using (var reader = new StreamReader(Path.Combine("Testcases", "test.kv")))
+    {
+        MainParser.FromKvFile(reader);
+        // JSON.DeserializeDynamic(reader);
+    }
+}
+catch (ParseException e)
+{
+    // ignored
+}
---
+++ 03-fuzz/fuzz-klementev/ParseLibrary/ParseException.cs
@@ -0,0 +1,8 @@
+namespace ParseLibrary;
+
+public class ParseException : Exception
+{
+    public ParseException(string message) : base(message)
+    {
+    }
+}
