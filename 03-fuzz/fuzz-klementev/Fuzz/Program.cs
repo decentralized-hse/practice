546ac3d9914ab355f79dd2b262984e2a500a5b51
Клементьев Матвей <klementevlogotip@mail.ru>
2023-10-30 01:57:11 +0500

    Инфраструктура для запуска фаззинга

--- 03-fuzz/fuzz-klementev/Fuzz/Program.cs
+++ 03-fuzz/fuzz-klementev/Fuzz/Program.cs
@@ -1,33 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using ParseLibrary;
+﻿using ParseLibrary;
 using SharpFuzz;
 
-// Fuzzer.OutOfProcess.Run(stream =>
-// {
-//     try
-//     {
-//         using (var reader = new StreamReader(stream))
-//         {
-//             MainParser.FromKvFile(reader);
-//             // JSON.DeserializeDynamic(reader);
-//         }
-//     }
-//     catch (ParseException e)
-//     {
-//     }
-// });
-
-
-try
+Fuzzer.OutOfProcess.Run(stream =>
 {
-    using (var reader = new StreamReader(Path.Combine("Testcases", "test.kv")))
+    try
     {
+        using var reader = new StreamReader(stream);
         MainParser.FromKvFile(reader);
-        // JSON.DeserializeDynamic(reader);
     }
-}
-catch (ParseException e)
-{
-    // ignored
-}
+    catch (ParseException)
+    {
+    }
+});
+
+
+// try
+// {
+//     using (var reader = new StreamReader(Path.Combine("Testcases", "test.kv")))
+//     {
+//         MainParser.FromKvFile(reader);
+//         // JSON.DeserializeDynamic(reader);
+//     }
+// }
+// catch (ParseException e)
+// {
+//     // ignored
+// }
