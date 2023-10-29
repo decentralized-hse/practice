477b5518c89b07ab4b7b3b9223a1b760d8520842
BootyOne <BootyOne@users.noreply.github.com>
2023-10-30 01:10:21 +0500

    fix

--- 01-git/get-romanenko/src/main.rs
+++ 01-git/get-romanenko/src/main.rs
@@ -1,49 +1,84 @@
-use std::env;
 use std::fs;
-use std::io;
-
-fn get(path: &str, root_hash: &str) -> io::Result<String> {
-    let parts: Vec<&str> = path.split('/').collect();
-
-    let mut current_hash = root_hash.to_string();
-
-    for part in parts {
-        let content = fs::read_to_string(&current_hash)?;
-
-        if let Some(line) = content.lines().find(|line| line.starts_with(part)) {
-            let parts: Vec<&str> = line.split('\t').collect();
-            if parts.len() < 2 {
-                return Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid data format"));
-            }
-            current_hash = parts[1].to_string();
-        } else {
-            return Err(io::Error::new(io::ErrorKind::NotFound, format!("{} not found", part)));
-        }
-    }
-
-    let content = fs::read_to_string(&current_hash)?;
-    Ok(content)
-}
+use sha2::Digest;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 4 {
-        eprintln!("Usage: {} <command> <path> <root_hash>", args[0]);
-        return;
+        eprintln!("Поданы неправильные аргументы на вход. Должно быть 3 аргумента");
+        std::process::exit(1);
     }
 
-    let command = &args[1];
-    let path = &args[2];
-    let root_hash = &args[3];
+    let path: Vec<&str> = args[2].split('/').collect();
+    let hash = &args[3];
 
-    match command.as_str() {
-        "get" => {
-            match get(path, root_hash) {
-                Ok(content) => println!("{}", content),
-                Err(e) => eprintln!("Error: {}", e),
+    let mut current_hash = hash.clone();
+    for (i, segment) in path.iter().enumerate() {
+        let file_content = match fs::read(&current_hash) {
+            Ok(content) => content,
+            Err(e) => {
+                eprintln!("Ошибка: {}", e);
+                std::process::exit(1);
+            }
+        };
+
+        let calculated_hash = format!("{:x}", sha2::Sha256::digest(&file_content));
+        if calculated_hash != current_hash {
+            eprintln!("Целостность была нарушена. Целостность Данного файла была нарушена");
+            std::process::exit(1);
+        }
+
+        if i == path.len() - 1 {
+            match find_file(&file_content, segment) {
+                Some(filename) => {
+                    if let Err(e) = print_file(&filename) {
+                        eprintln!("Ошибка: {}", e);
+                        std::process::exit(1);
+                    }
+                }
+                None => {
+                    eprintln!("Файл {} не найден", segment);
+                    std::process::exit(1);
+                }
+            }
+        } else {
+            match find_dir(&file_content, segment) {
+                Some(dirhash) => current_hash = dirhash,
+                None => {
+                    eprintln!("Директория {} не найдена", segment);
+                    std::process::exit(1);
+                }
             }
         }
-        _ => eprintln!("Unsupported command"),
     }
+}
+
+fn print_file(filename: &str) -> std::io::Result<()> {
+    let file_content = fs::read_to_string(filename)?;
+    for line in file_content.lines() {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+fn find_file(file_content: &[u8], filename: &str) -> Option<String> {
+    let content_str = String::from_utf8_lossy(file_content);
+    for line in content_str.lines() {
+        let parts: Vec<&str> = line.split(":\t").collect();
+        if parts.len() == 2 && parts[0].trim() == filename {
+            return Some(parts[1].trim().to_string());
+        }
+    }
+    None
+}
+
+fn find_dir(file_content: &[u8], dir_name: &str) -> Option<String> {
+    let content_str = String::from_utf8_lossy(file_content);
+    for line in content_str.lines() {
+        let parts: Vec<&str> = line.split("/\t").collect();
+        if parts.len() == 2 && parts[0].trim() == dir_name {
+            return Some(parts[1].trim().to_string());
+        }
+    }
+    None
 }
