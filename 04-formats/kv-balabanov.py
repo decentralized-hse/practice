30053674cc39ae08b4884f1706ec1e71a37dfd8b
deffer <ighor.naumov.2002mail@mail.ru>
2023-11-01 16:02:21 +0500

    find bug and fix it

--- 04-formats/kv-balabanov.py
+++ 04-formats/kv-balabanov.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import ast
 import struct
 
 from dataclasses import dataclass, asdict
@@ -41,12 +42,10 @@ class Student:
         for attr_name, attr_val in asdict(self).items():
             if attr_name == "_id":
                 continue
-            if attr_name == "mark":
-                attr_val = "{0:0.4f}".format(attr_val)
             if attr_name == "practice":
                 lines.extend([f"[{self._id}].{attr_name}.[{i}]: {attr_val[i]}" for i in range(len(attr_val))])
             else:
-                lines.append(f"[{self._id}].{attr_name.replace('_', '.')}: {attr_val}")
+                lines.append("[{}].{}: {}".format(self._id, attr_name.replace('_', '.'), repr(attr_val)))
 
         return "\n".join(lines) + "\n"
 
@@ -66,7 +65,7 @@ class Student:
             if item.startswith("["):
                 continue
             name.append(item)
-        
+
         return "_".join(name), value
 
     @classmethod
@@ -75,7 +74,7 @@ class Student:
         d = {}
         while i < len(student_struct):
             if "practice" in student_struct[i].split(":")[0]:
-                d["practice"] = cls.parse_practice(student_struct[i:i+8])
+                d["practice"] = cls.parse_practice(student_struct[i:i + 8])
                 i += 8
             else:
                 key, val = cls.parse_line(student_struct[i])
@@ -84,7 +83,7 @@ class Student:
                 elif key == "project_mark":
                     d[key] = int(val)
                 else:
-                    d[key] = val
+                    d[key] = ast.literal_eval(val)
                 i += 1
         return Student(_id=0, **d)
 
