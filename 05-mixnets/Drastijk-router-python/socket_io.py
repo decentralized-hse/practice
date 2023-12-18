0a5bd016ab64c0dbbd2bb1d7e8abe855aa7c97d3
Akergez <ragusseven@gmail.com>
2023-12-18 15:55:35 +0500

    store connections

--- 05-mixnets/Drastijk-router-python/socket_io.py
+++ 05-mixnets/Drastijk-router-python/socket_io.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from queue import Queue
 
 from abstractions import BaseIO
 
@@ -7,7 +8,7 @@ from abstractions import BaseIO
 class OurSocketIO(BaseIO):
     def __init__(self, host):
         super().__init__()
-        self.connections = {}
+        self.queues: {str:Queue} = {}
         self.port = 8000
         self.host = host
 
@@ -16,32 +17,43 @@ class OurSocketIO(BaseIO):
         thread.start()
 
     def send_message(self, message: bytes, address: str):
-        new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        new_socket.connect((address, 8000))
-        new_socket.send(message)
-        new_socket.close()
+        if address not in self.queues:
+            new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            new_socket.connect((address, 8000))
+            new_socket.send(message)
+        self.queues[address].put(message)
+
+    def client_handler(self, conn, address):
+        conn.send(str.encode('You are now connected to the replay server... Type BYE to stop'))
+        data = []
+        while True:
+            # receive data stream. it won't accept data packet greater than 1024 bytes
+            curdata = conn.recv(64)
+            data += curdata
+            if len(curdata) < 64:
+                if self.on_message:
+                    self.on_message(bytes(data), address)
+                while not self.queues[address].empty():
+                    conn.send(self.queues[address].popleft())
+
+    def accept_connections(self, ServerSocket):
+        Client, address = ServerSocket.accept()
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
+        self.queues[address] = Queue()
+        new_thread = threading.Thread(target=self.client_handler, args=(Client,address[0]))
+        new_thread.start()
 
     def start_server(self, host, port):
-        server_socket = socket.socket()  # get instance
-        # look closely. The bind() function takes tuple as argument
-        server_socket.bind((host, port))  # bind host address and port together
+        ServerSocket = socket.socket()
+        try:
+            ServerSocket.bind((host, port))
+        except socket.error as e:
+            print(str(e))
+        print(f'Server is listing on the port {port}...')
+        ServerSocket.listen()
 
-        # configure how many client the server can listen simultaneously
-        server_socket.listen(2)
         while True:
-            conn, address = server_socket.accept()  # accept new connection
-            data = []
-            while True:
-                # receive data stream. it won't accept data packet greater than 1024 bytes
-                curdata = conn.recv(64)
-                data += curdata
-                if len(curdata) < 64:
-                    # if data is not received break
-                    break
-
-            if self.on_message:
-                self.on_message(bytes(data), address[0])
-            conn.close()  # close the connection
+            self.accept_connections(ServerSocket)
 
     def subscribe(self, event):
         self.on_message = event
