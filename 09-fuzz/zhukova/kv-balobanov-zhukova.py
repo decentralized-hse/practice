71e60c02a26b105fc2b294f042205df160ee9b5d
Darya Zhukova <cherepashka2080@gmail.com>
2024-03-02 16:17:44 +0300

    Moved fuzzer & serializer into one file

--- 09-fuzz/zhukova/kv-balobanov-zhukova.py
+++ 09-fuzz/zhukova/kv-balobanov-zhukova.py
@@ -119,12 +119,7 @@ class Student:
             project_mark=struct.unpack("<B", student_struct[123:124])[0],
             mark=struct.unpack("<f", student_struct[124:128])[0])
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file", type=str)
-    filename = parser.parse_args().file
-
+def proceed_file(filename):
     if filename.endswith(".bin"):
         with open(filename, "rb") as in_file, open(f"{filename[:-4]}.kv", "w") as out_file:
             counter = 0
@@ -152,3 +147,74 @@ if __name__ == "__main__":
             print(f"written to {filename[:-3]}.bin...")
     else:
         raise Exception("Invalid file format")
+
+
+import os
+import sys
+import frelatage
+
+LOG_FILE = "out.log"
+FILE_NAME = "test"
+PROG = "kv-balobanov-zhukova.py"
+
+def prapare():
+    os.system(f"gcc ../bin-gritzko-check.c -o ./checker.o")
+
+def validate_input(file_name: str):
+    code = os.system(f"./checker.o {file_name} 2> {LOG_FILE}")
+    exit_status = os.WEXITSTATUS(code)
+    return exit_status == 0
+
+def encode_data(file_name: str):
+    print('! encoding: .kv -> .bin', file=sys.stderr)
+    code = os.system(f"python3 {PROG} serializer -f {file_name} > {LOG_FILE}")
+    exit_status = os.WEXITSTATUS(code)
+    if exit_status != 0:
+        os._exit(1)
+
+def decode_data(file_name: str):
+    print('! decoding: .bin -> .kv', file=sys.stderr)
+    code = os.system(f"python3 {PROG} serializer -f {file_name} > {LOG_FILE}")
+    exit_status = os.WEXITSTATUS(code)
+    if exit_status != 0:
+        os._exit(1)
+
+def kv_parser_round_trip_executor(data):
+    bin_fname = f"{FILE_NAME}.bin"
+    kv_fname = f"{FILE_NAME}.kv"
+    with open(bin_fname, "w") as f:
+        f.write(data)
+    if not validate_input(bin_fname):
+        return
+    try:
+        decode_data(bin_fname)
+        encode_data(kv_fname)
+    except:
+        os._exit(1)
+    with open(bin_fname, "r") as f:
+        if f.read() != data:
+            with open("sample", "w") as f:
+                f.write(data + "\nvs\n" + f.read())
+            os._exit(1)
+
+
+def run_fuzzer():
+    prapare()
+    input = frelatage.Input(value="initial_value")
+    f = frelatage.Fuzzer(kv_parser_round_trip_executor, [[input]])
+    f.fuzz()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                        prog='Fuzzer & KV serializer deserializer',
+                        description='Program that imitates `get` command')
+    parser.add_argument('mode', default="fuzzer", choices=["fuzzer", "serializer"])
+    parser.add_argument("-f", '--file', type=str)
+
+    args = parser.parse_args()
+    mode = args.mode
+    if mode == "fuzzer":
+        run_fuzzer()
+    elif mode == "serializer":
+        filename = args.file
+        proceed_file(filename)
--- 09-fuzz/zhukova/readme.md
+++ 09-fuzz/zhukova/readme.md
@@ -1,1 +1,11 @@
-Фаззер находится в `fuzz-zhukova.py`, а решение с исправлениями в `kv-balobanov-zhukova.py`
+### Фаззер и сериалайзер объединены в `kv-balobanov-zhukova.py`
+
+- Чтобы запустить фаззер:
+```bash
+python3 kv-balobanov-zhukova.py  fuzzer
+```
+
+- Чтобы запустить сериалайзер:
+```bash
+python3 kv-balobanov-zhukova.py  serializer -f <file>
+```
