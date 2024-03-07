777b78d8639751e1998ec58c7249ca74def35bfe
dkshtakin <shtakin55@gmail.com>
2024-03-07 14:32:34 +0300

    Send malformed input msg to stderr

--- 09-fuzz/xml-gamezardashvili-shtakin/src/lib.rs
+++ 09-fuzz/xml-gamezardashvili-shtakin/src/lib.rs
@@ -1,2 +1,2 @@
-// [dkshtakin] move format.rs to separate file in order to import it from fuz target
+// [dkshtakin] move format.rs to separate file in order to import it from fuzz target
 pub mod format;
