dd85dae7de0ee283910c3e5e314c143c9518e3cf
Kirill Shubnikov <ksshubnikov@avito.ru>
2024-03-09 17:27:24 +0300

    Shubnikov-09-json-rust-fuzz: attempt 1

---
+++ 09-fuzz/json_boloninid_shubnikov/src/main.rs
@@ -0,0 +1,33 @@
+use std::env;
+
+mod solve;
+
+fn main() {
+    let mut argv: Vec<String> = env::args().collect();
+
+    if argv.len() < 2 {
+        panic!("Cannot find path to file");
+    }
+
+    if argv[1].ends_with(".json") {
+        match solve::json_to_bin(&mut argv[1]) {
+            Ok(_) => {
+                println!("Ok!");
+            }
+            Err(err) => {
+                eprintln!("Error {}", err);
+                process::exit(-1);
+            }
+        }
+    } else {
+        match solve::bin_to_json(&mut argv[1]) {
+            Ok(_) => {
+                println!("Ok!");
+            }
+            Err(err) => {
+                eprintln!("Error {}", err);
+                process::exit(-1);
+            }
+        }
+    }
+}
