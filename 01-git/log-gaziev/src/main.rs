4ea33b76b21f2a41c745b04a03131c3e9928d305
Saidakbar Gaziev <si.gaziev@gmail.com>
2024-03-18 01:18:58 +0300

    remove duplicates from put/rm

--- 01-git/log-gaziev/src/main.rs
+++ 01-git/log-gaziev/src/main.rs
@@ -18,6 +18,13 @@ fn get_line_by_prefix(content: &Vec<String>, prefix: &str) -> String {
     String::new()
 }
 
+fn get_line_from_file(filename: &str, prefix: &str) -> String {
+    let file = fs::File::open(&filename).expect("failed to open file");
+    let reader = BufReader::new(file);
+    let content: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
+    get_line_by_prefix(&content, prefix)
+}
+
 fn print_contents(file: &String) {    
     if !fs::metadata(file).is_ok() {
         println!("broken hash {}", file);
@@ -42,24 +49,23 @@ fn main() {
         process::exit(1);
     }
     let mut hash = args[1].to_string();
+    let mut prev_commit = "".to_string();
     loop {
         let path = Path::new(&hash);
         if !path.exists() {
             println!("path does not exists");
             process::exit(1);
         }
-        let file = fs::File::open(&hash).expect("failed to open file");
-        let reader = BufReader::new(file);
-        let content: Vec<String> = reader.lines().map(|line| line.unwrap()).collect();
 
-        let parent = get_line_by_prefix(&content, PARENT_HASH);
+        let parent = get_line_from_file(&hash, PARENT_HASH);
         if parent.len() == 0 {
             break;
         }
 
-        let commit = get_line_by_prefix(&content, COMMIT_HASH);
-        if commit.len() != 0 {
+        let commit = get_line_from_file(&hash, COMMIT_HASH);
+        if commit.len() != 0 && commit != prev_commit {
             print_contents(&commit);
+            prev_commit = commit;
         }
         hash = parent.clone();
     }
