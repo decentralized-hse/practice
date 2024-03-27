38095b0d631b19f3a59a6f9fd916c5d17a50d8e0
Saidakbar Gaziev <si.gaziev@gmail.com>
2024-03-27 07:16:17 +0300

    fuzzing xml-fmt done

---
+++ 09-fuzz/fuzz-gaziev/FuzzBinToXml/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using XmlFmt;
+using SharpFuzz;
+
+public class Program {
+    // public static bool EqualFiles(string lhs, string rhs) {
+    //     byte[] lhsBytes = File.ReadAllBytes(lhs);
+    //     byte[] rhsBytes = File.ReadAllBytes(rhs);
+    //     if (lhsBytes.Length != rhsBytes.Length) {
+    //         return false;
+    //     }
+    //     for (int i = 0; i < lhsBytes.Length; ++i) {
+    //         if (lhsBytes[i] != rhsBytes[i]) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    public static void WriteBinaryFromString(string stream, string filename) {
+        BinaryWriter writer = new BinaryWriter(File.Open(filename, FileMode.Create));
+        writer.Write(Encoding.ASCII.GetBytes(stream));
+        writer.Close();
+    }
+
+    public static void Main(string[] args) {
+        Fuzzer.OutOfProcess.Run(stream => {
+            try {
+                WriteBinaryFromString(stream, "test.bin");
+                XmlFmtProgram.BinToXml("test.bin", "intra.xml");
+                XmlFmtProgram.XmlToBin("intra.xml", "result.bin");
+                if (!XmlFmt.FilesComporator.CompareBinFiles("test.bin", "result.bin")) {
+                    throw new ArgumentOutOfRangeException("Files are not equal!");
+                }
+            } catch (XmlFmt.ParseException) {
+                // handle expected exception
+                return;
+            }
+        });
+    }
+}
---
+++ 09-fuzz/fuzz-gaziev/FuzzXmlToBin/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using XmlFmt;
+using SharpFuzz;
+
+public class Program {
+    // public static bool EqualFiles(string lhs, string rhs) {
+    //     byte[] lhsBytes = File.ReadAllBytes(lhs);
+    //     byte[] rhsBytes = File.ReadAllBytes(rhs);
+    //     if (lhsBytes.Length != rhsBytes.Length) {
+    //         return false;
+    //     }
+    //     for (int i = 0; i < lhsBytes.Length; ++i) {
+    //         if (lhsBytes[i] != rhsBytes[i]) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    public static void WriteStreamFromString(string stream, string filename) {
+        StreamWriter writer = new StreamWriter(File.Open(filename, FileMode.Create));
+        writer.Write(stream);
+        writer.Close();
+    }
+
+    public static void Main(string[] args) {
+        Fuzzer.OutOfProcess.Run(stream => {
+            try {
+                WriteStreamFromString(stream, "test.xml");
+                XmlFmtProgram.XmlToBin("test.xml", "intra.bin");
+                XmlFmtProgram.BinToXml("intra.bin", "result.xml");
+                if (!XmlFmt.FilesComporator.CompareXmlFiles("test.xml", "result.xml")) {
+                    throw new ArgumentOutOfRangeException("Files are not equal!");
+                }
+            } catch (XmlFmt.ParseException) {
+                // handle expected exception
+                return;
+            }
+        });
+    }
+}
