b24c558970233580c1f6a4b27224f108f485d697
Anastasia Afanasyeva <afnastya1704@gmail.com>
2024-03-31 03:28:26 +0000

    fuzzing & fixed solution after fuzzing

--- 09-fuzz/protobuf-zhukov-afanasyeva/src/main.rs
+++ 09-fuzz/protobuf-zhukov-afanasyeva/src/main.rs
@@ -2,9 +2,13 @@ use prost::{
     bytes::{BufMut, BytesMut},
     Message,
 };
-use protobuf_zhukov::binformat::{Students, Student, Project};
+use protobuf_zhukov_afanasyeva::binformat::Students;
+use protobuf_zhukov_afanasyeva::{
+    read_students_from_bin_file,
+    write_students_to_bin_file
+};
 use core::panic;
-use std::{env, fs, io, string::String};
+use std::{env, fs, io};
 
 
 const EXT_BIN: &str = ".bin";
@@ -17,68 +21,11 @@ fn read_students_from_protobuf_file(f: &mut impl io::Read) -> Students {
     Students::decode(b.into_inner()).expect("[x] Can't decode proto file")
 }
 
-fn read_students_from_bin_file(f: &mut impl io::Read) -> Students {
-    println!("[*] Reading binary student data...");
-    let mut students = Students::default();
-    while let Ok(student) = read_student_from_bin_file(&mut *f) {
-        students.student.push(student);
-    }
-    students
-}
-
-fn read_student_from_bin_file(mut f: impl io::Read) -> Result<Student, io::Error> {
-    let mut name = vec![0u8; 32];
-    let mut login = vec![0u8; 16];
-    let mut group = vec![0u8; 8];
-    let mut practice = vec![0u8; 8];
-    let mut project_repo = vec![0u8; 59];
-    let mut project_mark = [0u8; 1];
-    let mut mark = [0u8; 4];
-
-    f.read_exact(&mut name)?;
-    f.read_exact(&mut login)?;
-    f.read_exact(&mut group)?;
-    f.read_exact(&mut practice)?;
-    f.read_exact(&mut project_repo)?;
-    f.read_exact(&mut project_mark)?;
-    f.read_exact(&mut mark)?;
-
-    Ok(Student{
-        name: String::from_utf8(name).unwrap(),
-        login: String::from_utf8(login).unwrap(),
-        group: String::from_utf8(group).unwrap(),
-        practice: practice,
-        project: Some(Project{
-            repo: String::from_utf8(project_repo).unwrap(),
-            mark: u32::from(project_mark[0]),
-        }),
-        mark: f32::from_le_bytes(mark),
-    })
-}
-
-
 fn write_students_to_protobuf_file(students: Students, f: &mut impl io::Write) {
     let mut buf: Vec<u8> = Vec::with_capacity(300);
     students.encode(&mut buf).unwrap();
     f.write_all(&buf).expect("[x] Can't write to proto file")
 }
-    
-fn write_students_to_bin_file(students: Students, f: &mut impl io::Write) {
-    for s in students.student {
-        f.write_all(s.name.as_bytes()).unwrap();
-        f.write_all(s.login.as_bytes()).unwrap();
-        f.write_all(s.group.as_bytes()).unwrap();
-        f.write_all(&s.practice).unwrap();
-        let project = match s.project {
-            Some(val) => val,
-            None => Project::default(),
-        };
-        f.write_all(project.repo.as_bytes()).unwrap();
-        f.write_all(&project.mark.to_le_bytes()[0..1]).unwrap();
-        f.write_all(&s.mark.to_le_bytes()).unwrap();
-    }
-}
-
 
 fn main() { 
     let argv: Vec<String> = env::args().collect();
@@ -86,8 +33,8 @@ fn main() {
 
     let input_filename = argv[1].clone();
 
-    let mut ext_from = EXT_BIN.clone();
-    let mut ext_to = EXT_PROTO.clone();
+    let mut ext_from = EXT_BIN;
+    let mut ext_to = EXT_PROTO;
     if input_filename.ends_with(EXT_PROTO) {
         (ext_from, ext_to) = (ext_to, ext_from);
     } else if !input_filename.ends_with(EXT_BIN) {
@@ -99,7 +46,7 @@ fn main() {
     let mut ifile = fs::File::open(input_filename).expect("[x] Can't open file");
     let students: Students;
     if ext_from == EXT_BIN {
-        students = read_students_from_bin_file(&mut ifile);
+        students = read_students_from_bin_file(&mut ifile).unwrap();
     } else {
         students = read_students_from_protobuf_file(&mut ifile);
     }
