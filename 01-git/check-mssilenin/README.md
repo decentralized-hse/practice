7b7e01005bf200a42971522555c96a31356fb1ee
xRaiMaNx <mrkenyfull@gmail.com>
2024-04-26 22:22:03 +0300

    Fix empty lines and add supporting not root dirs

--- 01-git/check-mssilenin/README.md
+++ 01-git/check-mssilenin/README.md
@@ -5,23 +5,15 @@ zig build-exe check.zig
 
 # Запуск
 ```
-./check <path> <root_hash>
+./check <dir> <start_hash>
 ```
 
 # Пример вывода корректного дерева (dir ./correct)
 ```
-commit path: correct/61ec58ece347e6ac22f5227c642410bfad51e33bd2ca57e1f9d5b85502fa4ddf
-hex of file hash:61ec58ece347e6ac22f5227c642410bfad51e33bd2ca57e1f9d5b85502fa4ddf
-file name: 61ec58ece347e6ac22f5227c642410bfad51e33bd2ca57e1f9d5b85502fa4ddf
-commit path: correct/ebdd7b80aa4838dc4585af7f7d47b643024be479822f6c59b7c6313ce8322173
-hex of file hash:ebdd7b80aa4838dc4585af7f7d47b643024be479822f6c59b7c6313ce8322173
-file name: ebdd7b80aa4838dc4585af7f7d47b643024be479822f6c59b7c6313ce8322173
-commit path: correct/ea57e88f130debd48188990f1175c6cd891ef11017c1145fbf07f39ec2503aa8
-hex of file hash:ea57e88f130debd48188990f1175c6cd891ef11017c1145fbf07f39ec2503aa8
-file name: ea57e88f130debd48188990f1175c6cd891ef11017c1145fbf07f39ec2503aa8
-commit path: correct/86012759befd37636df5cc94ad673676a336616acd8b6156ae1bfdaf77465924
-hex of file hash:86012759befd37636df5cc94ad673676a336616acd8b6156ae1bfdaf77465924
-file name: 86012759befd37636df5cc94ad673676a336616acd8b6156ae1bfdaf77465924
+checking commit hash; commit_hash=eac6b33b5eeae8d1e88a3343fc00e8e9c3a5ab120198aecd5c39665852dd0016
+checking commit hash; commit_hash=ebdd7b80aa4838dc4585af7f7d47b643024be479822f6c59b7c6313ce8322173
+checking commit hash; commit_hash=ea57e88f130debd48188990f1175c6cd891ef11017c1145fbf07f39ec2503aa8
+checking commit hash; commit_hash=86012759befd37636df5cc94ad673676a336616acd8b6156ae1bfdaf77465924
 All is correct!
 ```
 
--- 01-git/check-mssilenin/check.zig
+++ 01-git/check-mssilenin/check.zig
@@ -10,17 +10,17 @@ const ErrorIncorrectTree = error{
     IncorrectHash,
 };
 
-pub fn concatenateDirCommit(allocator: Allocator, root: []const u8, commit: []const u8) ![]const u8 {
-    const new_len = root.len + commit.len + 1;
+// pub fn concatenateDirCommit(allocator: Allocator, root: []const u8, commit: []const u8) ![]const u8 {
+//     const new_len = root.len + commit.len + 1;
 
-    var result = try allocator.alloc(u8, new_len);
+//     var result = try allocator.alloc(u8, new_len);
 
-    std.mem.copy(u8, result[0..root.len], root);
-    std.mem.copy(u8, result[root.len .. root.len + 1], "/");
-    std.mem.copy(u8, result[root.len + 1 .. new_len], commit);
+//     std.mem.copy(u8, result[0..root.len], root);
+//     std.mem.copy(u8, result[root.len .. root.len + 1], "/");
+//     std.mem.copy(u8, result[root.len + 1 .. new_len], commit);
 
-    return result;
-}
+//     return result;
+// }
 
 fn checkBufHash(buf: []const u8, expected: []const u8) !void {
     var hash: [HASH_SIZE]u8 = undefined;
@@ -28,7 +28,7 @@ fn checkBufHash(buf: []const u8, expected: []const u8) !void {
 
     var slice_hash = std.mem.bytesAsSlice(u8, &hash);
 
-    std.debug.print("hex of file hash:{}\nfile name: {s}\n", .{ std.fmt.fmtSliceHexLower(slice_hash), expected });
+    //std.debug.print("hex of file hash:{}\nfile name: {s}\n", .{ std.fmt.fmtSliceHexLower(slice_hash), expected });
 
     var hex_bytes: [HEX_SIZE]u8 = undefined;
     _ = try std.fmt.bufPrint(&hex_bytes, "{}", .{std.fmt.fmtSliceHexLower(slice_hash)});
@@ -41,47 +41,106 @@ fn checkBufHash(buf: []const u8, expected: []const u8) !void {
     }
 }
 
-fn readAndCheckCommit(allocator: Allocator, dir: []const u8, name: []const u8) ![]const u8 {
-    // read current commit
-    var commit_path = try concatenateDirCommit(allocator, dir, name);
-    std.debug.print("commit path: {s}\n", .{commit_path});
-    var file = try std.fs.cwd().openFile(commit_path, .{});
+fn readCommit(allocator: Allocator, name: []const u8) ![]const u8 {
+    var file = try std.fs.cwd().openFile(name, .{});
     const file_buffer = try file.readToEndAlloc(allocator, GB);
     file.close();
-
-    // check if hash of commit != name
-    try checkBufHash(file_buffer, name);
 
     return file_buffer;
 }
 
-fn checkFile(allocator: Allocator, root: []const u8, name: []const u8) !void {
-    var file_buffer = try readAndCheckCommit(allocator, root, name);
+fn readAndCheckCommit(allocator: Allocator, hash: []const u8) ![]const u8 {
+    // read current commit
+    std.debug.print("checking commit hash; commit_hash={s}\n", .{hash});
+    var file_buffer = try readCommit(allocator, hash);
+
+    // check if hash of commit != name
+    try checkBufHash(file_buffer, hash);
+
+    return file_buffer;
+}
+
+fn findDir(allocator: Allocator, root_dir: []const u8, commit_hash: []const u8) ![]const u8 {
+    var current_commit_hash = try allocator.alloc(u8, commit_hash.len);
+    std.mem.copy(u8, current_commit_hash, commit_hash);
+
+    if (std.mem.eql(u8, root_dir, ".")) {
+        return current_commit_hash;
+    }
+
+    var dirs_it = std.mem.split(u8, root_dir, "/");
+
+    while (dirs_it.next()) |dir| {
+        var file_buffer = try readCommit(allocator, current_commit_hash);
+        defer allocator.free(file_buffer);
+
+        // split commit by '\n'
+        var lines = std.mem.split(u8, file_buffer, "\n");
+        while (lines.next()) |line| {
+            // skip empty line
+            if (line.len == 0) {
+                continue;
+            }
+
+            // split line by '/\t'
+            var it = std.mem.split(u8, line, "/\t");
+            var commit_dir = it.next();
+
+            // file -> skip
+            if (it.peek() == null) {
+                continue;
+            }
+
+            // find commit_dir == dir
+            if (std.mem.eql(u8, dir, commit_dir.?)) {
+                allocator.free(current_commit_hash);
+
+                var hash = it.peek().?;
+                current_commit_hash = try allocator.alloc(u8, hash.len);
+                std.mem.copy(u8, current_commit_hash, hash);
+                break;
+            }
+        }
+    }
+
+    std.debug.print("found dir={s}\n", .{current_commit_hash});
+
+    return current_commit_hash;
+}
+
+fn checkFile(allocator: Allocator, name: []const u8) !void {
+    var file_buffer = try readAndCheckCommit(allocator, name);
     defer allocator.free(file_buffer);
 }
 
-fn checkDir(allocator: Allocator, root: []const u8, name: []const u8) !void {
-    var file_buffer = try readAndCheckCommit(allocator, root, name);
+fn checkDir(allocator: Allocator, name: []const u8) !void {
+    var file_buffer = try readAndCheckCommit(allocator, name);
     defer allocator.free(file_buffer);
 
     // split commit by '\n'
     var lines = std.mem.split(u8, file_buffer, "\n");
     while (lines.next()) |line| {
+        // skip empty line
+        if (line.len == 0) {
+            continue;
+        }
+
         // split line by '/\t'
         var it = std.mem.split(u8, line, "/\t");
-        _ = it.next(); // skip name
+        _ = it.next(); // skip name of dir
 
         // file (len(it) == 1)
         if (it.peek() == null) {
             var file_it = std.mem.split(u8, line, ":\t");
             _ = file_it.next(); // skip name of file
-            try checkFile(allocator, root, file_it.next().?);
+
+            try checkFile(allocator, file_it.next().?);
 
             continue;
         }
 
         // dir
-        try checkDir(allocator, root, it.next().?);
+        try checkDir(allocator, it.next().?);
     }
 }
 
@@ -91,14 +150,17 @@ pub fn main() !void {
     var args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
     if (args.len != 3) {
-        std.debug.print("Usage: ./check <root_path> <root_hash>\n", .{});
+        std.debug.print("Usage: ./check <dir> <start_hash>\n", .{});
         return;
     }
 
-    const root = args[1];
-    const root_hash = args[2];
+    const dir = args[1];
+    const start_hash = args[2];
 
-    try checkDir(allocator, root, root_hash);
+    var hash = try findDir(allocator, dir, start_hash);
+    defer allocator.free(hash);
+
+    try checkDir(allocator, hash);
 
     std.debug.print("All is correct!\n", .{});
 }
