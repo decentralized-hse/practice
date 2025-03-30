3df0cb920e1643ae107e506e9a4639028fdf624e
Arseny Bolotnikov <ars.bolotnikov93@gmail.com>
2025-03-30 17:49:33 +0300

    init signer

---
+++ rdx/con/con-send/go.mod
@@ -0,0 +1,3 @@
+module sender
+
+go 1.22.4
---
+++ rdx/con/con-send/signer.go
@@ -0,0 +1,87 @@
+package sender
+
+import (
+	"crypto/ecdsa"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/asn1"
+	"encoding/json"
+	"log"
+	"math/big"
+	"os"
+)
+
+type Transaction struct {
+	From   string
+	To     string
+	Amount int
+	Key    *ecdsa.PrivateKey
+}
+
+type txData struct {
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Amount int    `json:"amount"`
+}
+
+type signedTransaction struct {
+	Data      txData `json:"data"`
+	Signature []byte `json:"signature"`
+}
+
+func SignAndSend(tx Transaction) {
+
+	data := txData{
+		From:   tx.From,
+		To:     tx.To,
+		Amount: tx.Amount,
+	}
+
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalf("Error marshaling transaction data: %v", err)
+	}
+
+	hash := sha256.Sum256(dataBytes)
+
+	r, s, err := ecdsa.Sign(rand.Reader, tx.Key, hash[:])
+	if err != nil {
+		log.Fatalf("Error signing transaction: %v", err)
+	}
+
+	sig, err := asn1.Marshal(struct {
+		R *big.Int
+		S *big.Int
+	}{r, s})
+	if err != nil {
+		log.Fatalf("Error encoding signature: %v", err)
+	}
+
+	signedTx := signedTransaction{
+		Data:      data,
+		Signature: sig,
+	}
+
+	output, err := json.Marshal(signedTx)
+	if err != nil {
+		log.Fatalf("Error marshaling signed transaction: %v", err)
+	}
+
+	os.Stdout.Write(output)
+}
+
+// func main() {
+// 	privKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+
+// 	tx := Transaction{
+// 		From:   "Alice",
+// 		To:     "Bob",
+// 		Key:    privKey,
+// 		Amount: 100,
+// 	}
+
+// 	SignAndSend(tx)
+// }
